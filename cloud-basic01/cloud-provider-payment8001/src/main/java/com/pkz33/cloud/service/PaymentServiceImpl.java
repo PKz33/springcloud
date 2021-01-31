995735c36dfe23837dcb75d58b8a7b7df2f1da43
pkz33 <zhanghang_hz@163.com>
2021-01-31 23:02:12 +0800

    eureka & zookeeper & consul & ribbon

---
+++ cloud-basic01/cloud-provider-payment8001/src/main/java/com/pkz33/cloud/service/PaymentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pkz33.cloud.service;
+
+import com.pkz33.cloud.dao.PaymentDao;
+import com.pkz33.cloud.entities.Payment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
---
+++ cloud-basic01/cloud-provider-payment8002/src/main/java/com/pkz33/cloud/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.pkz33.cloud.service;
+
+import com.pkz33.cloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
