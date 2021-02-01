70a592296de0061f64064ce6d2c203e7ff3cf6f7
pkz33 <zhanghang_hz@163.com>
2021-02-01 10:58:18 +0800

    openfeign

---
+++ cloud-basic01/cloud-consumer-feign-order80/src/main/java/com/pkz33/cloud/cotroller/OrderFeignController.java
@@ -0,0 +1,23 @@
+package com.pkz33.cloud.cotroller;
+
+import com.pkz33.cloud.entities.CommonResult;
+import com.pkz33.cloud.entities.Payment;
+import com.pkz33.cloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+}
---
+++ cloud-basic01/cloud-consumer-feign-order80/src/main/java/com/pkz33/cloud/service/PaymentFeignService.java
@@ -0,0 +1,15 @@
+package com.pkz33.cloud.service;
+
+import com.pkz33.cloud.entities.CommonResult;
+import com.pkz33.cloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
