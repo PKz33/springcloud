8a5953bbf4de53337c953ee0097d3841b1b4873c
pkz33 <zhanghang_hz@163.com>
2021-02-07 09:57:31 +0800

    gateway

---
+++ cloud-basic01/cloud-gateway-gateway9527/src/main/java/com/pkz33/cloud/config/GateWayConfig.java
@@ -0,0 +1,16 @@
+package com.pkz33.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_pkz33", rou->rou.path("/baidu").uri("https://www.baidu.com/")).build();
+        return routes.build();
+    }
+}
